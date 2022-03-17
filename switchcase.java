5ad474f4382e0ffa9f6a23f009343d1913196f3c
Naveen <naveenkumar.gb@zensar.com>
2022-03-17 15:03:33 +0530

    switchcase

---
+++ switchcase.java
@@ -0,0 +1,20 @@
+public class switchcase {
+    public static void main(String[] args) {
+        int a = 7;
+        switch (a) {
+            case 1:
+            if (a>0) {
+                System.out.println("a is p");
+
+            }
+            break;
+            case 2 :
+            if (a<0) {
+            System.out.println(" a is n");  
+            }
+            break;
+            default:
+        }
+    }
+    
+}
