419e2c5a7343d0bd29ff550368f5c08c757d1cec
Naveen <naveenkumar.gb@zensar.com>
2022-03-17 13:34:24 +0530

    loops

---
+++ hello.java
@@ -0,0 +1,7 @@
+  public class hello  {
+    //runner debug
+    public static void main(String[] args)
+    {
+        System.out.println("king");
+    }
+}
---
+++ variables.java
@@ -0,0 +1,20 @@
+
+
+public class variables {
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        if(a > b) {
+            System.out.println("a is greater");
+        }
+            else {
+                System.out.println("AAAAAA");
+
+            }
+
+        }
+        
+        
+    }
+    
+
